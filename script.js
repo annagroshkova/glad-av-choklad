6933fdd963a79725d8d64b8b4bd3f4c70d4452ad
Anna Groshkova <anna.matvyeyenko@gmail.com>
2025-09-21 02:37:42 +0200

    second commit

--- script.js
+++
@@ -1,7 +0,0 @@
-const navbar = document.getElementById('navbar')
-
-window.addEventListener('scroll', () => {
-    if (window.scrollY > 5) {
-        navbar.classList.toggle('navbar_bg-color')
-    }
-})
